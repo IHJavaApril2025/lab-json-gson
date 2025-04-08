424e390fff71c12190bea5437f5d8c73ce4c0c51
Francisco Ismael Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-04-08 15:27:06 +0200

    interaccion 1: Clase Apartment

---
+++ maven-gson/src/main/java/com/example/Apartment.java
@@ -0,0 +1,92 @@
+package com.example;
+
+import java.util.UUID;
+
+public class Apartment {
+    private String id;
+    private String address;
+    private double price;
+    private int rooms;
+    private String description;
+    private String owner;
+    private boolean isAvailable;
+
+    public Apartment(String id, String address, double price, int rooms, String description, String owner, boolean isAvailable) {
+        this.id = UUID.randomUUID().toString();
+        this.address = address;
+        this.price = price;
+        this.rooms = rooms;
+        this.description = description;
+        this.owner = owner;
+        this.isAvailable = isAvailable;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(int rooms) {
+        this.rooms = rooms;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Apartment{" +
+                "id='" + id + '\'' +
+                ", address='" + address + '\'' +
+                ", price=" + price +
+                ", rooms=" + rooms +
+                ", description='" + description + '\'' +
+                ", owner='" + owner + '\'' +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
+}
