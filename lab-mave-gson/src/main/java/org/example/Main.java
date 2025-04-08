5c5be08581277e8f7aa076daf2852a38504c9619
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2025-04-08 18:44:24 +0200

    Lab Commit

---
+++ lab-mave-gson/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println();
+
+    }
+}
